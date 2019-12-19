c405e6dfc06f0bcbfae2c9eb622c8d3dec06a952
fadhlanfm <fafadhlanriz@gmail.com>
2019-12-19 16:48:28 +0700

    first commit

---
+++ exercise-5/2_indexAccessing.js
@@ -0,0 +1,12 @@
+var word = 'wow JavaScript is so cool';
+var exampleFirstWord = word[0] + word[1] + word[2];
+var secondWord = word[3] + word[4] + word[5] + word[6]+ word[7]+ word[8]+ word[9]+ word[10]+ word[11]+ word[12]+ word[13]; // do your own!
+var thirdWord= word[14] + word[15] + word[16]; // do your own!
+var fourthWord = word[17] + word[18] + word[19]; // do your own!
+var fifthWord = word[20] + word[21] + word[22] + word[23]+ word[24]; // do your own!
+
+console.log('First Word: ' + exampleFirstWord);
+console.log('Second Word: ' + secondWord);
+console.log('Third Word: ' + thirdWord);
+console.log('Fourth Word: ' + fourthWord);
+console.log('Fifth Word: ' + fifthWord);
