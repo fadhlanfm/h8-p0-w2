e928aa129222d1b1b292f6aea67ec1323a9f480f
fadhlanfm <fafadhlanriz@gmail.com>
2019-12-19 18:02:37 +0700

    first commit

--- exercise-7/3_menyusunTanggaBintang.js
+++ exercise-7/3_menyusunTanggaBintang.js
@@ -2,14 +2,15 @@ var rows3 = 5; // input the number of rows
 var output = [];
 var batas = 1;
 var k = 0;
+var batas = 1;
+
 
 // do loops to display asterisks in the console.
-
-for(i=0;i<rows3;i++){
-    for(j=0;j<rows2;j++){
+    for(j=0;j<rows3;j++){
         while(k < batas){
             output[k]="*";
+            k++;
         }
+        console.log(output);
+        batas++;
       }
-  console.log(output);
-}
