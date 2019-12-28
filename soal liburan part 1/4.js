83371203deeb75024781ebf101e27f55ecca2648
fadhlanfm <fafadhlanriz@gmail.com>
2019-12-28 13:50:34 +0700

    first commit

--- soal liburan part 1/4.js
+++ soal liburan part 1/4.js
@@ -16,8 +16,108 @@ var array = [
 console.log(getCoordinates(array))
 // ["B1", "A3"]
 */
+function konversiJ(j){
+  switch(j){
+    case 1:
+      return 'A';
+      break;
+    case 2:
+      return 'B';
+      break;
+    case 3:
+      return 'C';
+      break;
+    case 4:
+      return 'D';
+      break;
+    case 5:
+      return 'E';
+      break;
+    case 6:
+      return 'F';
+      break;    
+    case 7:
+      return 'G';
+      break;
+    case 8:
+      return 'H';
+      break;
+    case 9:
+      return 'I';
+      break;
+    case 10:
+      return 'J';
+      break;
+    case 11:
+      return 'K';
+      break;
+    case 12:
+      return 'L';
+      break;
+    case 13:
+      return 'M';
+      break;
+    case 14:
+      return 'N';
+      break;
+    case 15:
+      return 'O';
+      break;
+    case 16:
+      return 'P';
+      break;
+    case 17:
+      return 'Q';
+      break;
+    case 18:
+      return 'R';
+      break;
+    case 19:
+      return 'S';
+      break;
+    case 20:
+      return 'T';
+      break;
+    case 21:
+      return 'U';
+      break;
+    case 22:
+      return 'V';
+      break;
+    case 23:
+      return 'W';
+      break;
+    case 24:
+      return 'X';
+      break;
+    case 25:
+      return 'Y';
+      break;
+    case 26:
+      return 'Z';
+      break;
+    default:
+      return 'error';
+  }
+}
+
 function getCoordinates(arr) {
   // code below here
+  var jumlahKolom = arr[0].length;
+  var jumlahBaris = arr.length;
+  var output = [];
+  var indexOutput = 0;
+
+  for(i=1;i<jumlahBaris;i++){
+    for(j=1;j<jumlahKolom;j++){
+      
+      if(arr[i][j]=='x'){
+        output[indexOutput]= konversiJ(j) +''+ i;
+        indexOutput++;
+      }
+    }
+  }
+  return output;
 }
 
 var array = [
